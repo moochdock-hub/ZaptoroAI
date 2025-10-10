5d91dad250deca2f471f89cccdd3a8c4b38bca18
oldspice13 <mecsupreme1@gmail.com>
2025-10-09 22:21:11 -0700

    chore(cleanup): remove temp/dev artifacts from repo

--- tools/extract_delays.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync(path.join(__dirname, '..', 'ZAPTOROAI.html'),'utf8');
-const re = /animation-delay:\s*([\d.]+s)/gi;
-const set = new Set();
-let m;
-while ((m = re.exec(html)) !== null) set.add(m[1]);
-console.log(Array.from(set).sort().join('\n'));
